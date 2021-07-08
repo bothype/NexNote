3cd2c0b05d15128aa2bcd815f0973f7cf22e0a87
hyperior099 <86219203+hyperior099@users.noreply.github.com>
2021-07-08 14:59:52 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+  $(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').delay(3500).fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(3500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(3500).css({'overflow':'visible'});
+ })
+//]]>
