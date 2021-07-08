b420d435dcfb699d1a6fe3828e2ca42f9be1c9c9
hyperior099 <86219203+hyperior099@users.noreply.github.com>
2021-07-08 15:00:44 +0530

    Update frost.js

--- frost.js
+++ frost.js
@@ -29,8 +29,3 @@ body {
 <div id="status">&nbsp;</div>
 </div>
 ");
-  $(window).on('load', function() { // makes sure the whole site is loaded 
-    $('#status').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(350).css({'overflow':'visible'});
- })
